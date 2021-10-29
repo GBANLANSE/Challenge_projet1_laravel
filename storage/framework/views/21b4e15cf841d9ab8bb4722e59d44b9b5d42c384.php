a9c314103239b5f7caf81121ea13517f0089f785
GBANLANSE <88885250+GBANLANSE@users.noreply.github.com>
2021-10-29 09:49:35 +0000

    Add files via upload

---
+++ storage/framework/views/21b4e15cf841d9ab8bb4722e59d44b9b5d42c384.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Ma page a props</h1>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\Projet1-laravel\resources\views/apropos.blade.php ENDPATH**/ ?>
